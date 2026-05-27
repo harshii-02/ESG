1220ebb4367e5ac434b0d5750adede097db107b0
harshii-02 <saiharshithak02@gmail.com>
2026-05-27 22:08:47 +0530

    Redirect Vite assets on deployment

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic import RedirectView
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("ingestion.urls")),
+    re_path(
+        r"^assets/(?P<path>.*)$",
+        RedirectView.as_view(url="/static/assets/%(path)s", permanent=False),
+    ),
     re_path(r"^(?!api/|admin/).*", TemplateView.as_view(template_name="index.html")),
 ]
-
